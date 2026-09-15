0ebe704ab409474993e241f5356cf46ce51d4768
حسين رائد السامعي <abwsmwdaltmymy@gmail.com>
2026-09-15 21:38:12 +0300

    first commit

--- Form1.cs
+++ Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace frist_project
@@ -15,6 +9,20 @@ namespace frist_project
         public Form1()
         {
             InitializeComponent();
+
+            // إعدادات النافذة الرئيسية
+            this.Text = "أمان اليمن - المنصة الرئيسية";
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.WindowState = FormWindowState.Maximized;
+            this.MinimumSize = new Size(1100, 650);
+            this.BackColor = Color.FromArgb(15, 23, 42);
+            this.RightToLeft = RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // ضع كود تحميل البيانات هنا لاحقاً
         }
     }
 }
